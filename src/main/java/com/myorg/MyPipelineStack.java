55b77280438d5cdadf3e60ba0c0d1a6ca5ce743d
Matthew Knight <matthew.knight@standardbank.co.za>
2023-01-25 11:49:28 +0200

    initial commit

---
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -0,0 +1,33 @@
+package com.myorg;
+
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.ShellStep;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+
+import java.util.Arrays;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class MyPipelineStack extends Stack {
+    public MyPipelineStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public MyPipelineStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+
+
+        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+                .pipelineName("MyPipeline")
+                .synth(ShellStep.Builder.create("Synth")
+                        .input(CodePipelineSource.gitHub("kngmat004/my-pipeline", "main"))
+                        .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
+                        .build())
+                .build();
+    }
+}
